72be325453d4ce2b15a23f960f666e80d0dbeac8
Gerardo Larramendy <nuevogrardo@gmail.com>
2020-08-12 14:48:32 -0300

    Le cambiamos el login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,16 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
+    function login(user, pass){
+        if(user.trim()==="" || pass.trim()===""){
+            alert("Debe rellenar los datos para continuar");
+        }else{
+            localStorage.setItem("usuario", user.trim());
+            localStorage.setItem("contraseña", pass.trim());
+            location.href="home.html";
+        }
+    }
+
+
+document.addEventListener("DOMContentLoaded", function(e){});
