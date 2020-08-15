0a6ee3028e59a4495a26fce2cd4efb09603cddec
Gerardo Larramendy <nuevogrardo@gmail.com>
2020-08-15 17:15:37 -0300

    Cambiados los scripts de estos dos archivos, agregada la lista

--- js/products.js
+++ js/products.js
@@ -1,6 +1,49 @@
+var categoriesArray = [];
+showSpinner();
+function showCategoriesList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                    
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <br>
+                        <br>
+                        
+                        <small class="text-muted">` + category.soldCount + ` artículos vendidos</small>
+                        
+                    </div>
+                    <p class="mb-1">`+ category.description +`</p><br>
+                    <p class="mb-1">`+ category.currency +`  `+ category.cost +`</p>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    hideSpinner();
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+        }
+    });
 });
